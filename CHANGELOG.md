b137b9fed4e10545ad285b7233565984968624b4
Gedy Palomino <gedy.palomino@gmail.com>
2021-04-26 00:57:16 -0500

    refactor(extension): use three top-level functions instead of class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [v3](https://github.com/gedzeppelin/monitor-window-switcher/compare/v2...v3) (2021-04-26)
+
+### Refactor
+
+- **extension:** use three top-level functions instead of class.
+
 ## [v2](https://github.com/gedzeppelin/monitor-window-switcher/compare/v1...v2) (2021-04-25)
 
 ### Features
--- extension.js
+++ extension.js
@@ -7,11 +7,24 @@ const Main = imports.ui.main;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-
 const Prefs = Me.imports.prefs;
 
+// Original references
+let _getWindows;
+let _windowSwitcherPopup;
+
+// Settings
+let settings;
+let filterPerMonitorSignal;
+let showInCurrentSignal;
 let FILTER_PER_MONITOR = true;
 let SHOW_IN_CURRENT = true;
+
+// Modified
+let getWindows;
+let windowSwitcherPopup;
+
+//
 
 function getSchema() {
   let schemaDir = Me.dir.get_child("schemas").get_path();
@@ -25,74 +38,67 @@ function getSchema() {
   return new Gio.Settings({ settings_schema: schema });
 }
 
-class Extension {
-  settings;
+function enableSettings() {
+  const { FILTER_PER_MONITOR_KEY, SHOW_IN_CURRENT_KEY } = Prefs.Fields;
 
-  // Modified
-  getWindows;
-  windowSwitcherPopup;
+  FILTER_PER_MONITOR = settings.get_boolean(FILTER_PER_MONITOR_KEY);
+  SHOW_IN_CURRENT = settings.get_boolean(SHOW_IN_CURRENT_KEY);
 
-  // Original references
-  _getWindows;
-  _windowSwitcherPopup;
+  filterPerMonitorSignal = settings.connect(`changed::${FILTER_PER_MONITOR_KEY}`, () => {
+    FILTER_PER_MONITOR = settings.get_boolean(FILTER_PER_MONITOR_KEY);
+    AltTab.getWindows = FILTER_PER_MONITOR ? getWindows : _getWindows;
+  });
 
-  constructor() {
-    this.initSettings();
-
-    this._getWindows = AltTab.getWindows;
-    this._windowSwitcherPopup = AltTab.WindowSwitcherPopup;
-
-    this.getWindows = (workspace) => {
-      const monitor = global.display.get_current_monitor();
-      return this._getWindows(workspace)
-        .filter((w) => w.get_monitor() === monitor);
-    };
-
-    this.windowSwitcherPopup = GObject.registerClass(
-      class AlTabPopup extends AltTab.WindowSwitcherPopup {
-        vfunc_allocate() {
-          const primaryMonitor = Main.layoutManager.primaryMonitor;
-          Main.layoutManager.primaryMonitor = Main.layoutManager.currentMonitor;
-
-          const result = super.vfunc_allocate.apply(this, arguments);
-          Main.layoutManager.primaryMonitor = primaryMonitor;
-
-          return result;
-        }
-      }
-    );
-  }
-
-  enable() {
-    AltTab.getWindows = FILTER_PER_MONITOR ? this.getWindows : this._getWindows;
-    AltTab.WindowSwitcherPopup = SHOW_IN_CURRENT ? this.windowSwitcherPopup : this._windowSwitcherPopup;
-  }
-
-  disable() {
-    AltTab.getWindows = this._getWindows;
-    AltTab.WindowSwitcherPopup = this._windowSwitcherPopup;
-  }
-
-  initSettings() {
-    this.settings = getSchema();
-
-    const { FILTER_PER_MONITOR_KEY, SHOW_IN_CURRENT_KEY } = Prefs.Fields;
-
-    FILTER_PER_MONITOR = this.settings.get_boolean(FILTER_PER_MONITOR_KEY);
-    SHOW_IN_CURRENT = this.settings.get_boolean(SHOW_IN_CURRENT_KEY);
-
-    this.settings.connect(`changed::${FILTER_PER_MONITOR_KEY}`, () => {
-      FILTER_PER_MONITOR = this.settings.get_boolean(FILTER_PER_MONITOR_KEY);
-      AltTab.getWindows = FILTER_PER_MONITOR ? this.getWindows : this._getWindows;
-    });
-
-    this.settings.connect(`changed::${SHOW_IN_CURRENT_KEY}`, () => {
-      SHOW_IN_CURRENT = this.settings.get_boolean(SHOW_IN_CURRENT_KEY);
-      AltTab.WindowSwitcherPopup = SHOW_IN_CURRENT ? this.windowSwitcherPopup : this._windowSwitcherPopup;
-    });
-  }
+  showInCurrentSignal = settings.connect(`changed::${SHOW_IN_CURRENT_KEY}`, () => {
+    SHOW_IN_CURRENT = settings.get_boolean(SHOW_IN_CURRENT_KEY);
+    AltTab.WindowSwitcherPopup = SHOW_IN_CURRENT ? windowSwitcherPopup : _windowSwitcherPopup;
+  });
 }
 
+function disableSettings() {
+  settings.disconnect(filterPerMonitorSignal);
+  settings.disconnect(showInCurrentSignal);
+}
+
+//
+
 function init() {
-  return new Extension();
+  _getWindows = AltTab.getWindows;
+  _windowSwitcherPopup = AltTab.WindowSwitcherPopup;
+
+  settings = getSchema();
+
+  getWindows = function (workspace) {
+    const monitor = global.display.get_current_monitor();
+    return _getWindows(workspace)
+      .filter((w) => w.get_monitor() === monitor);
+  };
+
+  windowSwitcherPopup = GObject.registerClass(
+    class AlTabPopup extends AltTab.WindowSwitcherPopup {
+      vfunc_allocate() {
+        const primaryMonitor = Main.layoutManager.primaryMonitor;
+        Main.layoutManager.primaryMonitor = Main.layoutManager.currentMonitor;
+
+        const result = super.vfunc_allocate.apply(this, arguments);
+        Main.layoutManager.primaryMonitor = primaryMonitor;
+
+        return result;
+      }
+    }
+  );
+}
+
+function enable() {
+  enableSettings();
+
+  AltTab.getWindows = FILTER_PER_MONITOR ? getWindows : _getWindows;
+  AltTab.WindowSwitcherPopup = SHOW_IN_CURRENT ? windowSwitcherPopup : _windowSwitcherPopup;
+}
+
+function disable() {
+  AltTab.getWindows = _getWindows;
+  AltTab.WindowSwitcherPopup = _windowSwitcherPopup;
+
+  disableSettings();
 }
